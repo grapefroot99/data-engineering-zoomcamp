2b360ba035f7a2362e796237826ecb806f81ce2d
Aaron Michael <aaronmichaelariedo@gmail.com>
2026-07-24 10:27:08 +0000

    Added comments and notes for study

---
+++ module-1/pipeline/README.md
@@ -0,0 +1,15 @@
+***Not fully organized
+
+Files and folders used to learn about docker containers, docker compose, postgre and everything in module 1 of the course
+Also used virtual environments through uv(?) and jupyter notebook among other things
+
+
+Main files are 
+- docker-compose.yaml
+- Dockerfile
+- ingest-data.py (made from notebook.ipynb)
+- pyproject.toml
+- commands.txt (for running the docker containers)
+(Some have notes and comments for studying)
+
+Everything else are mainly for practice or redundant
--- module-1/pipeline/ingest_data.py
+++ module-1/pipeline/ingest_data.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+import click                            # for parameters/props in cli docker run
+import pandas as pd                     # read csv and manipulate data
+from sqlalchemy import create_engine    # connects to postgres database
+from tqdm.auto import tqdm              # chunking the data
 
-import click
-import pandas as pd
-from sqlalchemy import create_engine
-from tqdm.auto import tqdm
-
+# lists all data types on unclean csv
 dtype = {
     "VendorID": "Int64",
     "passenger_count": "Int64",
@@ -27,12 +26,14 @@ dtype = {
     "congestion_surcharge": "float64"
 }
 
+# same as above but for dates
 parse_dates = [
     "tpep_pickup_datetime",
     "tpep_dropoff_datetime"
 ]
 
 
+# click command line interface to run the script with parameters
 @click.command()
 @click.option('--pg-user', default='root', help='PostgreSQL user')
 @click.option('--pg-pass', default='root', help='PostgreSQL password')
@@ -40,17 +41,22 @@ parse_dates = [
 @click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
 @click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
 @click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+
+# actual ingestion function
 def run(pg_user, pg_pass, pg_host, pg_port, pg_db, target_table):
-    year = 2021
-    month = 1
+    year = 2021             # arbitrary for taxi data file
+    month = 1               # arbitrary for taxi data file
 
-    chunksize = 100000
+    chunksize = 100000      # arbitrary for iterating and chucking the data
 
+    # data source prefix and url
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
     url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
 
+    # connects to the postgres database using sqlalchemy and parameters from click
     engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')    
 
+    # reads the csv in chunks
     df_iter = pd.read_csv(
         url,
         dtype=dtype,
@@ -59,22 +65,21 @@ def run(pg_user, pg_pass, pg_host, pg_port, pg_db, target_table):
         chunksize = chunksize,
     )
 
+    # iterates through the chunks and writes them to the postgres database
     first = True
     
     for df_chunk in tqdm(df_iter):
+        # first chunk creates the table and headers
         if first:
             df_chunk.head(0).to_sql(name=target_table, con=engine, if_exists='replace')
             first = False
 
+        # actual rows of data
         df_chunk.to_sql(name=target_table, con=engine, if_exists='append')
 
 if __name__ == '__main__':
     run()
 
-
-
-
-# In[ ]:
 
 
 
